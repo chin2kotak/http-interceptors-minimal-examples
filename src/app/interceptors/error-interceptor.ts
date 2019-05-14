7f43016c510801b962f1e71e2277e5cdf7fd00bc
chandan kotak <chandankotak@qdata.io>
2019-05-14 11:38:00 +0530

    little corrections & adjustments

--- src/app/interceptors/error-interceptor.ts
+++ src/app/interceptors/error-interceptor.ts
@@ -1,29 +1,22 @@
-import { HttpEvent, 
-         HttpInterceptor, 
-         HttpHandler, 
-         HttpRequest, 
-         HttpResponse,
-         HttpErrorResponse} from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 export class HttpErrorInterceptor  implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
     return next.handle(request)
-               .pipe(
-                 catchError( (error: HttpErrorResponse) => {   
-                   console.log('JSK', error);
-                    let errMsg = '';
-                    // Client Side Error
-                    if (error.error instanceof ErrorEvent) {		
-                      errMsg = `Error Hardi: ${error.error.message}`;
-                    } 
-                    else {  // Server Side Error
-                      errMsg = `Error Code Hardi: ${error.status},  Message: ${error.message}`;
-                    }
-                    // return an observable
-                    return throwError(errMsg);
-                  })
-               )
+      .pipe(
+        catchError( (error: HttpErrorResponse) => {   
+          let errMsg = '';
+          // Our Error
+          if (error.error instanceof ErrorEvent) {		
+            errMsg = `Error: ${error.error.message}`;
+          } 
+          else {  // Error From Server
+            errMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+          }
+          return throwError(errMsg);
+        })
+      )
   }
 } 
--- src/app/interceptors/httpconfig.interceptor.ts
+++ src/app/interceptors/httpconfig.interceptor.ts
@@ -1,19 +1,12 @@
-import { HttpEvent, 
-         HttpInterceptor, 
-         HttpHandler, 
-         HttpRequest, 
-         HttpResponse,
-         HttpErrorResponse} from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 export class HttpConfigInterceptor  implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-    console.log('request', );
-    if(request.url.indexOf('flipkart') == -1) { 
+    const conditionForBypass = request.url.indexOf('flipkart') == -1;
+    if(conditionForBypass) { 
       request = request.clone({ headers: request.headers.set('Authorization', 'LAKHAN') });
     }
-  
     return next.handle(request);
   }
 } 
