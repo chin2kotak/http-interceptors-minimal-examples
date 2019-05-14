c0a19ee5b2d87bd60a763e78512694be3b8d9be4
chandan kotak <chandankotak@qdata.io>
2019-05-14 11:42:52 +0530

    little corrections & adjustments

--- src/app/interceptors/httpconfig.interceptor.ts
+++ src/app/interceptors/httpconfig.interceptor.ts
@@ -5,7 +5,7 @@ export class HttpConfigInterceptor  implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
     const conditionForBypass = request.url.indexOf('flipkart') == -1;
     if(conditionForBypass) { 
-      request = request.clone({ headers: request.headers.set('Authorization', 'LAKHAN') });
+      request = request.clone({ headers: request.headers.set('Authorization', 'XYZ') });
     }
     return next.handle(request);
   }
